9c57d8aa719e91a24d9efa2a1e096bfb6dfecd2c
28gomon <28gomon@gmail.com>
2020-10-19 15:49:43 +0300

    Updates

---
+++ precache-manifest.693408a87a188b6cae7a86ab8fa46d31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820e37e6dd3daecd1c34d01a2ecf2c90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9865577f7a67eafadc99",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "8bdb29496c20300c14f6",
+    "url": "/static/css/main.6c43a32b.chunk.css"
+  },
+  {
+    "revision": "9865577f7a67eafadc99",
+    "url": "/static/js/2.862b8edf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.862b8edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdb29496c20300c14f6",
+    "url": "/static/js/main.7ca65840.chunk.js"
+  },
+  {
+    "revision": "229b6e1d1f38a1141206",
+    "url": "/static/js/runtime-main.c472db4c.js"
+  },
+  {
+    "revision": "ac8ade58b7abf5f5c48dbf032f21e8ff",
+    "url": "/static/media/card-1.ac8ade58.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
